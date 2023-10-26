cd728bfa6ddebe7210d5a901cd97ddec257da1f1
archstation <ejovo13@yahoo.com>
2023-10-26 21:54:41 +0200

    add run script

--- cuda_build.sh
+++ cuda_build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+
+# Build this cuda project from the root directory of a google colab notebook.
 mkdir -p build
 cd build
 cmake ../Monte-Carlo-Project-CUDA
---
+++ cuda_run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# Run this project's default program from the root directory of a google colab notebook
+cd build
+./main
