daeba7df0bf91298e66954cd39d417186c6ffaa6
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-23 23:48:29 +0100

    d

--- hello.cu
+++ hello.cu
@@ -138,7 +138,7 @@ int main(void) {
     printf("Do we have ECC memory? %d\n", prop.ECCEnabled);
 
 // declare variables and constants
-    const size_t N_PATHS = 10;
+    const size_t N_PATHS = 8;
     const size_t N_STEPS = 5;
     const size_t N_NORMALS = N_PATHS*N_STEPS;
     const float T = 1.0f;
