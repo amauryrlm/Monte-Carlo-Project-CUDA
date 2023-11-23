3613b5c7b8515b254d6c6d45076b1906bfc07ee4
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-23 20:30:06 +0100

    up

--- hello.cu
+++ hello.cu
@@ -49,195 +49,137 @@ __global__ void simulateOptionPrice(float *d_optionPriceGPU, float K, float r, f
         // }
         
         // // Calculate the payoff
-        // d_optionPriceGPU[idx] = 1.0f;
+        d_optionPriceGPU[idx] = 1.0f;
     }
 }
 
 
 
-// int main(void) {
+int main(void) {
 
-// // declare variables and constants
-//     const size_t N_PATHS = 10;
-//     const size_t N_STEPS = 5;
-//     const size_t N_NORMALS = N_PATHS*N_STEPS;
-//     const float T = 1.0f;
-//     const float K = 100.0f;
-//     const float B = 95.0f;
-//     const float S0 = 100.0f;
-//     const float sigma = 0.2f;
-//     const float mu = 0.1f;
-//     const float r = 0.05f;
-//     float dt = float(T)/float(N_STEPS);
-//     float sqrdt = sqrt(dt);
+// declare variables and constants
+    const size_t N_PATHS = 10;
+    const size_t N_STEPS = 5;
+    const size_t N_NORMALS = N_PATHS*N_STEPS;
+    const float T = 1.0f;
+    const float K = 100.0f;
+    const float B = 95.0f;
+    const float S0 = 100.0f;
+    const float sigma = 0.2f;
+    const float mu = 0.1f;
+    const float r = 0.05f;
+    float dt = float(T)/float(N_STEPS);
+    float sqrdt = sqrt(dt);
 
-//     vector<float> s(N_PATHS);
+    vector<float> s(N_PATHS);
 
-//     float step = 1.0f / N_STEPS;
-//     float G = 0.0f;
-//     std::default_random_engine generator;
-//     std::normal_distribution<double> distribution(0.0, 1.0);
+    float step = 1.0f / N_STEPS;
+    float G = 0.0f;
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, 1.0);
 
-//     cout << "step : " << step << endl;
-//     G = distribution(generator);
-//     // cout << "G : " << G;
-
-
+    cout << "step : " << step << endl;
+    G = distribution(generator);
+    // cout << "G : " << G;
 
 
 
 
-//     // generate random numbers using curand
-
-//     //allocate array filled with random values 
-//     float *d_randomData;
-//     testCUDA(cudaMalloc(&d_randomData, N_PATHS * N_STEPS * sizeof(float)));
-
-//     // create generator all fill array with generated values
-//     curandGenerator_t gen;
-//     curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
-//     curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
-//     curandGenerateNormal(gen, d_randomData, N_PATHS * N_STEPS, 0.0, 1.0);
-
-//     cout << "number generated";
 
 
-//     float h_randomData[N_PATHS * N_STEPS];
-//     testCUDA(cudaMemcpy(h_randomData, d_randomData, N_PATHS * N_STEPS * sizeof(float), cudaMemcpyDeviceToHost));
+    // generate random numbers using curand
 
-//     cout << "host copied" << endl;
-//     cout << h_randomData[0];
+    //allocate array filled with random values 
+    float *d_randomData;
+    testCUDA(cudaMalloc(&d_randomData, N_PATHS * N_STEPS * sizeof(float)));
 
-//     // for(int i = 0; i < N_PATHS * N_STEPS; i++) {
-//     //     cout << "random  : " << h_randomData[i] << endl;
-//     // }
+    // create generator all fill array with generated values
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
+    curandGenerateNormal(gen, d_randomData, N_PATHS * N_STEPS, 0.0, 1.0);
 
-//     float count = 0.0f;
-//     for(int i=0; i<N_PATHS;i++){
-//         float St = S0;
-//         for(int j=0; j<N_STEPS; j++){
-//             G = h_randomData[i*j];
-//             // cout << "G : " << G << endl;
-//             St *= exp((r - (sigma*sigma)/2)*dt + sigma * sqrdt * G);
+    cout << "number generated";
+
+
+    float h_randomData[N_PATHS * N_STEPS];
+    testCUDA(cudaMemcpy(h_randomData, d_randomData, N_PATHS * N_STEPS * sizeof(float), cudaMemcpyDeviceToHost));
+
+    cout << "host copied" << endl;
+    cout << h_randomData[0];
+
+    // for(int i = 0; i < N_PATHS * N_STEPS; i++) {
+    //     cout << "random  : " << h_randomData[i] << endl;
+    // }
+
+    float count = 0.0f;
+    for(int i=0; i<N_PATHS;i++){
+        float St = S0;
+        for(int j=0; j<N_STEPS; j++){
+            G = h_randomData[i*j];
+            // cout << "G : " << G << endl;
+            St *= exp((r - (sigma*sigma)/2)*dt + sigma * sqrdt * G);
             
-//         }
-//         // cout << "S before assigning " << St << endl;
-//         s[i] = St;
-//         count += St;
-//         cout << "St : " << St << endl;
-//         // cout << "S " << St << endl;
-//         // cout << i << endl;
-//     }
-//     cout << "paths calculated" << endl;
-//     cout << "mean paths : " << count/N_PATHS << endl;
+        }
+        // cout << "S before assigning " << St << endl;
+        s[i] = St;
+        count += St;
+        cout << "St : " << St << endl;
+        // cout << "S " << St << endl;
+        // cout << i << endl;
+    }
+    cout << "paths calculated" << endl;
+    cout << "mean paths : " << count/N_PATHS << endl;
 
 
 
-//     // simulateOptionPrice<<<1, N_PATHS>>>( d_optionPriceGPU,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt);
-//     // cudaDeviceSynchronize();
-//     float *d_optionPriceGPU;
-//     testCUDA(cudaMalloc(&d_optionPriceGPU,N_PATHS*sizeof(float)));
-
-//     int blockSize = 256; // You can adjust this based on your GPU's capability
-//     int numBlocks = (N_PATHS + blockSize - 1) / blockSize;
-
-//     cout << "nb block" << numBlocks << endl;
-
-//     initializeArray<<<numBlocks, blockSize>>>(d_optionPriceGPU, N_PATHS, 6.0f);
-//     cudaError_t err = cudaGetLastError();
-//     if (err != cudaSuccess) {
-//         cout << "CUDA Error: " << cudaGetErrorString(err) << endl;
-//     }
-//     testCUDA(cudaDeviceSynchronize());
-//     float *h_optionPriceGPU = new float[N_PATHS];
-//     testCUDA(cudaMemcpy(h_optionPriceGPU, d_optionPriceGPU,N_PATHS*sizeof(float),cudaMemcpyDeviceToHost));
-
-//     for(int i = 0; i<N_PATHS; i++){
-//         cout << "GPU St : " << h_optionPriceGPU[i] << endl;
-//     }
-//     // cout << "mean paths GPU : " << mean_priceGPU/N_PATHS << endl;
+    // simulateOptionPrice<<<1, N_PATHS>>>( d_optionPriceGPU,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt);
+    // cudaDeviceSynchronize();
 
 
-//     testCUDA(cudaFree(d_randomData));
-//     curandDestroyGenerator(gen);
+    float *a;
+    a = (float *)malloc(N_PATHS * sizeof(float));
+    float *d_a;
+    d_a = testCUDA(cudaMalloc((void **)&d_a,N_PATHS*sizeof(float)));
+    cudaMemcpy(d_a, a, N_PATHS * sizeof(int), cudaMemcpyHostToDevice);
 
-// 	return 0;
-// }
+    simulateOptionPrice<<<1, N_PATHS>>>( d_a,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt);
+    cudaDeviceSynchronize();
 
-#include <stdio.h>
+    cudaMemcpy(a, d_a, length * sizeof(float), cudaMemcpyDeviceToHost);
 
-#define NB 16384
-#define NTPB 1024
-
-
-
-// Has to be defined in the compilation in order to get the correct value of the macros
-// __FILE__ and __LINE__
-
-__device__ void Test(int *a) {
-
-	for (int i = 0; i < 1000; i++) {
-		*a = *a + 1;
-	}
-}
-
-__device__ int aGlob[NB*NTPB];					// Global variable solution
-
-__global__ void MemComp(int *a){
-
-	int idx = threadIdx.x + blockIdx.x*blockDim.x;
-
-	aGlob[idx] = a[idx];
-
-	Test(aGlob + idx);
-
-	a[idx] = aGlob[idx];
-}
+    for(int i = 0; i<N_PATHS; i++){
+        cout << "GPU St : " << a[i] << endl;
+    }
 
 
-int main (void){
+    // float *d_optionPriceGU;
+    // testCUDA(cudaMalloc(&d_optionPriceGPU,N_PATHS*sizeof(float)));
 
-	
-	int *a, *aGPU;
-	float Tim;										// GPU timer instructions
-	cudaEvent_t start, stop;						// GPU timer instructions
-	testCUDA(cudaEventCreate(&start));				// GPU timer instructions
-	testCUDA(cudaEventCreate(&stop));				// GPU timer instructions
-		
-	a = (int*)malloc(NB*NTPB*sizeof(int));
-	testCUDA(cudaMalloc(&aGPU, NB*NTPB * sizeof(int)));
+    // int blockSize = 256; // You can adjust this based on your GPU's capability
+    // int numBlocks = (N_PATHS + blockSize - 1) / blockSize;
 
-	for(int i=0; i<NB; i++){
-		for(int j=0; j<NTPB; j++){
-			a[j+i*NTPB] = j+i*NTPB;
-		}
-	}
+    // cout << "nb block" << numBlocks << endl;
 
-	testCUDA(cudaMemcpy(aGPU, a, NB*NTPB*sizeof(int), cudaMemcpyHostToDevice));
+    // initializeArray<<<numBlocks, blockSize>>>(d_optionPriceGPU, N_PATHS, 6.0f);
+    // cudaError_t err = cudaGetLastError();
+    // if (err != cudaSuccess) {
+    //     cout << "CUDA Error: " << cudaGetErrorString(err) << endl;
+    // }
+    // testCUDA(cudaDeviceSynchronize());
+    // float *h_optionPriceGPU = new float[N_PATHS];
+    // testCUDA(cudaMemcpy(h_optionPriceGPU, d_optionPriceGPU,N_PATHS*sizeof(float),cudaMemcpyDeviceToHost));
 
-	testCUDA(cudaEventRecord(start, 0));			// GPU timer instructions
-	
-	for(int i = 0; i<100; i++) {
-		MemComp<<<NB,NTPB>>>(aGPU);
-	}
-	
-	testCUDA(cudaEventRecord(stop, 0));				// GPU timer instructions
-	testCUDA(cudaEventSynchronize(stop));			// GPU timer instructions
-	testCUDA(cudaEventElapsedTime(&Tim,				// GPU timer instructions
-		start, stop));								// GPU timer instructions
+    // for(int i = 0; i<N_PATHS; i++){
+    //     cout << "GPU St : " << h_optionPriceGPU[i] << endl;
+    // }
+    // cout << "mean paths GPU : " << mean_priceGPU/N_PATHS << endl;
 
-	printf("Time per execution: %f ms\n", Tim/100);
-	testCUDA(cudaEventDestroy(start));				// GPU timer instructions
-	testCUDA(cudaEventDestroy(stop));				// GPU timer instructions
 
-	testCUDA(cudaMemcpy(a, aGPU, NB*NTPB*sizeof(int), cudaMemcpyDeviceToHost));
-	testCUDA(cudaFree(aGPU));
-
-	for(int i= 0; i<4; i++){
-		printf("%i = %i \n", 100000 + i, a[i]);
-	}
-
-	free(a);
+    testCUDA(cudaFree(d_randomData));
+    curandDestroyGenerator(gen);
 
 	return 0;
 }
+
+
