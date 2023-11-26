9c69365cd073f0166cb570eff132a6c1d5201f61
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-26 13:01:53 +0100

    up

--- hello.cu
+++ hello.cu
@@ -424,7 +424,12 @@ int main(void) {
     simulateOptionPriceGPU<<<1, 1024>>>( d_optionPriceGPU,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt);
     // simulateOptionPriceGPUSumReduce<<<1, N_PATHS>>>( d_optionPriceGPU,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt, d_output);
     simulateOptionPriceOneBlockGPUSumReduce<<<1, N_PATHS>>>( d_optionPriceGPU,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt, d_output);
-
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
+        // Handle the error (e.g., by exiting the program)
+        return -1;
+    }
     cudaDeviceSynchronize();
 
 
