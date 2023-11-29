769286f9546f46e4fe0df51fd56678951b348a4a
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-29 11:40:12 +0100

    up

--- hello.cu
+++ hello.cu
@@ -509,7 +509,7 @@ int main(void) {
 
     cout << endl;
     float sum = 0.0f;
-    for(int i=0; i<N_STEPS; i++){
+    for(int i=0; i<N_PATHS; i++){
         cout << "gpu2 " << i << h_optionPriceGPU2[i] << endl;
     }
 
