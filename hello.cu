30d4c441ef8ed32e278405e97681fb4d45c06eee
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-29 12:12:14 +0100

    up

--- hello.cu
+++ hello.cu
@@ -428,7 +428,7 @@ int main(void) {
 
 
 // declare variables and constants
-    unsigned int N_PATHS = 10;
+    unsigned int N_PATHS = 2048;
     const size_t N_STEPS = 365;
     const float T = 1.0f;
     const float K = 100.0f;
