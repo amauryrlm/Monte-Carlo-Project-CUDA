f6f3522c87a1f9ae374e99a135c9d831bdd1a011
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-29 11:52:56 +0100

    up

--- hello.cu
+++ hello.cu
@@ -418,7 +418,7 @@ int main(void) {
 
 // declare variables and constants
     const size_t N_PATHS = 10;
-    const size_t N_STEPS = 1000;
+    const size_t N_STEPS = 365;
     const float T = 1.0f;
     const float K = 100.0f;
     const float B = 95.0f;
