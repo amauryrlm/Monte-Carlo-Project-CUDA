56af15eb624adef37b4279de5728885fdf27e723
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-29 14:43:16 +0100

    up

--- hello.cu
+++ hello.cu
@@ -65,8 +65,49 @@ static double CND(double d)
     return cnd;
 }
 
+// __global__ void reduce3(float *g_idata, float *g_odata, unsigned int n) {
+//   // Handle to thread block group
+//   extern __shared__ float sdata[1024];
+
+//   // perform first level of reduction,
+//   // reading from global memory, writing to shared memory
+//   unsigned int tid = threadIdx.x;
+//   unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+
+//   float mySum = (i < n) ? g_idata[i] : 0;
+  
+
+//   if (i + blockDim.x < n) mySum += g_idata[i + blockDim.x];
+//   __syncthreads();
+//   sdata[tid] = mySum;
+
+
+
+//   // do reduction in shared mem
+//   for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+//     if (tid < s) {
+//         mySum = mySum + sdata[tid + s];
+//         sdata[tid] = mySum;
+
+//     }
+
+//     __syncthreads();
+
+//   }
+
+
+//   // write result for this block to global mem
+//   if (tid == 0){
+//     printf("mySum last %f , %d \n", mySum, blockIdx.x);
+//     g_odata[blockIdx.x] = mySum;
+
+//   } 
+// }
+
+
 __global__ void reduce3(float *g_idata, float *g_odata, unsigned int n) {
   // Handle to thread block group
+  cg::thread_block cta = cg::this_thread_block();
   extern __shared__ float sdata[1024];
 
   // perform first level of reduction,
@@ -78,7 +119,7 @@ __global__ void reduce3(float *g_idata, float *g_odata, unsigned int n) {
   
 
   if (i + blockDim.x < n) mySum += g_idata[i + blockDim.x];
-  __syncthreads();
+  cg::sync(cta);
   sdata[tid] = mySum;
 
 
@@ -91,7 +132,7 @@ __global__ void reduce3(float *g_idata, float *g_odata, unsigned int n) {
 
     }
 
-    __syncthreads();
+    cg::sync(cta);
 
   }
 
@@ -102,6 +143,49 @@ __global__ void reduce3(float *g_idata, float *g_odata, unsigned int n) {
     g_odata[blockIdx.x] = mySum;
 
   } 
+}
+
+
+
+__global__ void reduce4(T *g_idata, T *g_odata, unsigned int n) {
+  // Handle to thread block group
+  cg::thread_block cta = cg::this_thread_block();
+  T *sdata = SharedMemory<T>();
+
+  // perform first level of reduction,
+  // reading from global memory, writing to shared memory
+  unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+
+  T mySum = (i < n) ? g_idata[i] : 0;
+
+  if (i + blockSize < n) mySum += g_idata[i + blockSize];
+
+  sdata[tid] = mySum;
+  cg::sync(cta);
+
+  // do reduction in shared mem
+  for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1) {
+    if (tid < s) {
+      sdata[tid] = mySum = mySum + sdata[tid + s];
+    }
+
+    cg::sync(cta);
+  }
+
+  cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
+
+  if (cta.thread_rank() < 32) {
+    // Fetch final intermediate sum from 2nd warp
+    if (blockSize >= 64) mySum += sdata[tid + 32];
+    // Reduce final warp using shuffle
+    for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
+      mySum += tile32.shfl_down(mySum, offset);
+    }
+  }
+
+  // write result for this block to global mem
+  if (cta.thread_rank() == 0) g_odata[blockIdx.x] = mySum;
 }
 
 
