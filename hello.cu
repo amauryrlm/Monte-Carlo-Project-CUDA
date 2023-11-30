75c1ab0294dcfc140aa583234ba7e640d95b570d
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-11-30 13:56:08 +0100

    up

--- hello.cu
+++ hello.cu
@@ -97,7 +97,7 @@ __global__ void reduce3(float *g_idata, float *g_odata, unsigned int n) {
 
   // write result for this block to global mem
   if (tid == 0){
-    // printf("mySum last %f , %d \n", mySum, blockIdx.x);
+    printf("mySum last %f , %d \n", mySum, blockIdx.x);
 
     g_odata[blockIdx.x] = mySum;
 
