6c8f385a13d2055f7012ad44dd8d6066008da93a
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-06 20:50:52 +0100

    up

--- hello.cu
+++ hello.cu
@@ -8,21 +8,12 @@
 #include <math.h>
 #include "BlackandScholes.hpp"
 #include "reduce.cuh"
+#include "tool.cuh"
 
 using namespace std;
 
 
-extern "C" bool isPow2(unsigned int x) { return ((x & (x - 1)) == 0); }
 
-unsigned int nextPow2(unsigned int x) {
-  --x;
-  x |= x >> 1;
-  x |= x >> 2;
-  x |= x >> 4;
-  x |= x >> 8;
-  x |= x >> 16;
-  return ++x;
-}
 
 // Function that catches the error
 void testCUDA(cudaError_t error, const char *file, int line) {
@@ -262,55 +253,6 @@ __global__ void simulateBulletOptionPriceMultipleBlockGPU(float *d_simulated_pay
 }
 
 
-void getDeviceProperty(){
-
-    int count;
-    cudaDeviceProp prop;
-    cudaGetDeviceCount(&count);
-    printf("The number of devices available is %i GPUs \n", count);
-    cudaGetDeviceProperties(&prop, count-1);
-    printf("Name: %s\n", prop.name);
-    printf("Global memory size in bytes: %ld\n", prop.totalGlobalMem);
-    printf("Shared memory size per block: %ld\n", prop.sharedMemPerBlock);
-    printf("Number of registers per block: %d\n", prop.regsPerBlock);
-    printf("Number of threads in a warp: %d\n", prop.warpSize);
-    printf("Maximum number of threads that can be launched per block: %d\n", prop.maxThreadsPerBlock);
-    printf("Maximum number of threads that can be launched: %d x %d x %d\n",
-           prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
-    printf("Maximum grid size: %d x %d x %d\n",
-           prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
-    printf("Total constant memory: %ld\n", prop.totalConstMem);
-    printf("Major compute capability: %d\n", prop.major);
-    printf("Minor compute capability: %d\n", prop.minor);
-    printf("Clock rate: %d\n", prop.clockRate);
-    printf("Maximum 1D texture memory: %d\n", prop.maxTexture1D);
-    printf("Could we overlap? %d\n", prop.deviceOverlap);
-    printf("Number of multiprocessors: %d\n", prop.multiProcessorCount);
-    printf("Is there a limit for kernel execution? %d\n", prop.kernelExecTimeoutEnabled);
-    printf("Is my GPU a chipsest? %d\n", prop.integrated);
-    printf("Can we map the host memory? %d\n", prop.canMapHostMemory);
-    printf("Can we launch concurrent kernels? %d\n", prop.concurrentKernels);
-    printf("Do we have ECC memory? %d\n", prop.ECCEnabled);
-
-}
-
-void simulateOptionPriceCPU(float *optionPriceCPU, int N_PATHS, int N_STEPS, float * h_randomData, float S0, float sigma, float sqrdt, float r, float K, float dt, float *simulated_paths_cpu){
-    float G;
-    float countt = 0.0f;
-    for(int i=0; i<N_PATHS;i++){
-        float St = S0;
-        for(int j=0; j<N_STEPS; j++){
-            G = h_randomData[i*N_STEPS + j];
-            St *= expf((r - (sigma*sigma)/2)*dt + sigma * sqrdt * G);
-            
-        }
-
-        simulated_paths_cpu[i] = max(St - K, 0.0f);
-        // cout << "cpu : " <<  St << endl;
-        countt += max(St - K, 0.0f);
-    }
-    *optionPriceCPU = expf(-r) * (countt/N_PATHS);
-}
 
 __global__ void simulateBulletOptionSavePrice(float *d_simulated_paths,float *d_simulated_count, float K, float r, float T,float sigma, int N_PATHS, float *d_randomData, int N_STEPS, float S0, float dt, float sqrdt, float B, float P1, float P2) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
