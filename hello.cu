96d661b828bb2be49f1d540785734b4f4fdbd8cb
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-06 23:05:17 +0100

    up

--- hello.cu
+++ hello.cu
@@ -286,7 +286,7 @@ int main(void) {
 
 
 // declare variables and constants
-    unsigned int N_PATHS = 100000000;
+    unsigned int N_PATHS = 10000000;
     const size_t N_STEPS = 100;
     const float T = 1.0f;
     const float K = 155.0;
@@ -302,7 +302,7 @@ int main(void) {
     int P2 = 50;
 
     int block_sizes [6] = {32, 64, 128, 256, 512, 1024};
-    int number_of_simulations [7] = {100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+    int number_of_simulations [7] = {100, 1000, 10000, 100000, 1000000, 10000000};
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
