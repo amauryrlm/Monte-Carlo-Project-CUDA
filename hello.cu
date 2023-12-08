d8fcd2fb6fe43ecf8a2f52e74b820add36f02fb5
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-08 16:25:46 +0100

    up

--- hello.cu
+++ hello.cu
@@ -50,60 +50,6 @@ __global__ void setup_kernel(curandState *state, uint64_t seed) {
     curand_init(seed, tid, 0, &state[tid]);
 }
 
-// __global__ void bullet_option_outter_trajectories_kernel(float *d_option_price, float *d_option_count) {
-//     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-//     float K = d_OptionData.K;
-//     float r = d_OptionData.r;
-//     float sigma = d_OptionData.v;
-//     int N_PATHS = d_OptionData.N_PATHS;
-//     float B = d_OptionData.B;
-//     int P1 = d_OptionData.P1;
-//     int P2 = d_OptionData.P2;
-//     float S0 = d_OptionData.S0;
-//     float dt = d_OptionData.step;
-//     float sqrdt = sqrtf(dt);
-//     int N_STEPS = d_OptionData.N_STEPS;
-//     float St = S0;
-//     float G;
-
-
-//     int count = 0;
-//     for (int i = 0; i < N_STEPS; i++) {
-//         G = curand_normal(&state);
-//         St *= expf((r - (sigma * sigma) / 2) * dt + sigma * sqrdt * G);
-//         if (B > St) count += 1;
-//     }
-//     if ((count >= P1) && (count <= P2)) {
-//         d_option_price[idx] = max(St - K, 0.0f);
-//     } else {
-//         d_option_price[idx] = 0.0f;
-//     }
-//     d_option_count[idx] = count;
-// }
-
-// int bullet_option_NMC_wrapper(int thread_per_block, OptionData op) {
-
-//     int blocksPerGrid = (op.N_PATHS + thread_per_block - 1) / thread_per_block;
-
-//     float *d_option_price, *d_option_count;
-//     testCUDA(cudaMalloc((void **) &d_option_price, op.N_PATHS * sizeof(float)));
-//     testCUDA(cudaMalloc((void **) &d_option_count, op.N_PATHS * sizeof(float)));
-
-//     bullet_option_outter_trajectories_kernel<<<blocksPerGrid, thread_per_block>>>(d_option_price, d_option_count);
-//     cudaError_t error = cudaGetLastError();
-//     if (error != cudaSuccess) {
-//         fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
-//         return -1;
-//     }
-//     cudaDeviceSynchronize();
-
-
-//     cudaFree(d_option_price);
-//     cudaFree(d_option_count);
-//     return 0;
-
-// }
-
 
 __global__ void simulateOptionPriceGPU(float *d_optionPriceGPU, float K, float r, float T, float sigma, int N_PATHS,
                                        float *d_randomData, int N_STEPS, float S0, float dt, float sqrdt) {
@@ -191,8 +137,6 @@ __global__ void simulateOptionPriceMultipleBlockGPUwithReduce(float *g_odata, cu
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int tid = threadIdx.x;
     int blockSize = blockDim.x;
-    curandState state = globalStates[idx];
-
     float K = d_OptionData.K;
     float r = d_OptionData.r;
     float T = d_OptionData.T;
@@ -255,25 +199,79 @@ __global__ void simulateOptionPriceMultipleBlockGPUwithReduce(float *g_odata, cu
 
 
 __global__ void
-simulateBulletOptionPriceMultipleBlockGPU(float *d_simulated_payoff, float K, float r, float T, float sigma,
-                                          int N_PATHS, float *d_randomData, int N_STEPS, float S0, float dt,
-                                          float sqrdt, float B, float P1, float P2) {
+simulateBulletOptionPriceMultipleBlockGPU(float *g_odata, curandState *globalStates) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
+    float S0 = d_OptionData.S0;
+    float T = d_OptionData.T;
+    float K = d_OptionData.K;
+    float r = d_OptionData.r;
+    float sigma = d_OptionData.v;
+    float B = d_OptionData.B;
+    int P1 = d_OptionData.P1;
+    int P2 = d_OptionData.P2;
+    int N_PATHS = d_OptionData.N_PATHS;
+    int N_STEPS = d_OptionData.N_STEPS;
+    float dt = d_OptionData.step;
+    float sqrdt = sqrtf(dt);
+
+    cg::thread_block cta = cg::this_thread_block();
+    __shared__ float sdata[1024];
+
+
     if (idx < N_PATHS) {
+        curandState state = globalStates[idx];
         int count = 0;
         float St = S0;
         float G;
         for (int i = 0; i < N_STEPS; i++) {
-            G = d_randomData[idx * N_STEPS + i];
+            G = curand_normal(&state);
             St *= expf((r - (sigma * sigma) / 2) * dt + sigma * sqrdt * G);
             if (B > St) count += 1;
         }
         if ((count >= P1) && (count <= P2)) {
-            d_simulated_payoff[idx] = max(St - K, 0.0f);
+            sdata[idx] = max(St - K, 0.0f);
         } else {
-            d_simulated_payoff[idx] = 0.0f;
+            sdata[idx] = 0.0f;
         }
+        cg::sync(cta);
+
+        if ((blockSize >= 1024) && (tid < 512)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 512];
+        }
+        cg::sync(cta);
+        if ((blockSize >= 512) && (tid < 256)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 256];
+        }
+
+        cg::sync(cta);
+
+        if ((blockSize >= 256) && (tid < 128)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 128];
+        }
+
+        cg::sync(cta);
+
+        if ((blockSize >= 128) && (tid < 64)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 64];
+        }
+        cg::sync(cta);
+
+
+        cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
+
+        if (cta.thread_rank() < 32) {
+            // Fetch final intermediate sum from 2nd warp
+            if (blockSize >= 64) mySum += sdata[tid + 32];
+            // Reduce final warp using shuffle
+            for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
+                mySum += tile32.shfl_down(mySum, offset);
+            }
+        }
+
+        // write result for this block to global mem
+        if (cta.thread_rank() == 0) g_odata[blockIdx.x] = mySum;
+
     }
 }
 
@@ -416,6 +414,42 @@ float wrapper_gpu_option_vanilla(OptionData option_data, int threadsPerBlock) {
     return optionPriceGPU;
 }
 
+float wrapper_gpu_bullet_option(OptionData option_data, int threadsPerBlock) {
+
+    int N_PATHS = option_data.N_PATHS;
+    int blocksPerGrid = (option_data.N_PATHS + threadsPerBlock - 1) / threadsPerBlock;
+    // generate states array for random number generation
+    curandState *d_states;
+    testCUDA(cudaMalloc(&d_states, N_PATHS * sizeof(curandState)));
+    setup_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_states, 1234);
+
+    float *d_odata;
+    testCUDA(cudaMalloc(&d_odata, blocksPerGrid * sizeof(float)));
+    float *h_odata = (float *) malloc(blocksPerGrid * sizeof(float));
+
+    simulateBulletOptionPriceMultipleBlockGPU<<<blocksPerGrid, threadsPerBlock>>>(d_odata, d_states);
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
+        return -1;
+    }
+    cudaDeviceSynchronize();
+    testCUDA(cudaMemcpy(h_odata, d_odata, blocksPerGrid * sizeof(float), cudaMemcpyDeviceToHost));
+    float sum = 0.0f;
+    for (int i = 0; i < blocksPerGrid; i++) {
+        sum += h_odata[i];
+    }
+    float optionPriceGPU = expf(-option_data.r * option_data.T) * sum / N_PATHS;
+    cout << "Average GPU bullet option : " << optionPriceGPU << endl << endl;
+
+    free(h_odata);
+    cudaFree(d_odata);
+    cudaFree(d_states);
+    return optionPriceGPU;
+
+
+}
+
 
 int main(void) {
 
@@ -432,17 +466,11 @@ int main(void) {
     option_data.N_STEPS = 100;
     option_data.step = option_data.T / static_cast<float>(option_data.N_STEPS);
 
+    int threadsPerBlock = 1024;
 
     // Copy option data to constant memory
     cudaMemcpyToSymbol(d_OptionData, &option_data, sizeof(OptionData));
     printOptionData(option_data);
-
-
-    int threadsPerBlock = 1024;
-    unsigned int maxThreads = 1024;
-
-
-    float times_for_simulations[6];
 
     getDeviceProperty();
 
@@ -450,144 +478,6 @@ int main(void) {
 
     wrapper_gpu_option_vanilla(option_data, threadsPerBlock);
 
-
-
-//--------------------------------GPU WITH MULTIPLE BLOCK ----------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------
-    // float milliseconds = 0.0f;
-    // float mean = 0.0f;
-    // for(int i = 0; i < 6; i++){
-    //   for(int j = 0; j < 3; j++){
-    //     for(int k = 0; k < 10; k++){
-
-    //       cudaEventCreate(&start);
-    //       cudaEventCreate(&stop);
-
-    //       threads = block_sizes[i];
-    //       N_PATHS = number_of_simulations[j];
-    //       blocks = (N_PATHS + (threads * 2 - 1)) / (threads * 2);
-    //       blocksPerGrid = (N_PATHS + threads - 1) / threads;
-
-    //       cout << endl << "number of paths : " << N_PATHS << endl;
-    //       cout << "number of threads : " << threads << endl;
-
-    //       cout << "number of blocks : " << blocks << endl;
-    //       cout << "number of blocks per grid : " << blocksPerGrid << endl;
-
-
-
-    //       float *output3, *d_optionPriceGPU3, *d_output3;
-    //       output3 = (float *)malloc(blocks * sizeof(float));
-
-    //       testCUDA(cudaMalloc((void **)&d_optionPriceGPU3,N_PATHS*sizeof(float)));
-    //       testCUDA(cudaMalloc((void **)&d_output3,blocks * sizeof(float)));
-    //       //start time
-
-
-    //       testCUDA(cudaEventRecord(start));
-
-    //       simulateOptionPriceMultipleBlockGPU<<<blocksPerGrid,threads>>>( d_optionPriceGPU3,  K,  r,  T, sigma,  N_PATHS,  d_randomData,  N_STEPS, S0, dt, sqrdt);
-    //       cudaError_t error3 = cudaGetLastError();
-    //       if (error3 != cudaSuccess) {
-    //           fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error3));
-    //           return -1;
-    //       }
-    //       cudaError_t err;
-    //       err = cudaEventRecord(stop);
-    //       if (err != cudaSuccess) {
-    //           fprintf(stderr, "Failed to record stop event (error code %s)!\n", cudaGetErrorString(err));
-    //           exit(EXIT_FAILURE);
-    //       }
-    //       cudaEventSynchronize(stop);
-    //       cudaEventElapsedTime(&milliseconds, start, stop);
-    //       mean += milliseconds;
-
-
-
-    //       reduce6<<<blocks,threads>>>(d_optionPriceGPU3,d_output3,N_PATHS, isPow2(N_PATHS));
-    //       error3 = cudaGetLastError();
-    //       if (error3 != cudaSuccess) {
-    //           fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error3));
-    //           return -1;
-    //       }
-
-
-
-
-    //       testCUDA(cudaMemcpy(output3, d_output3, blocks * sizeof(float), cudaMemcpyDeviceToHost));
-
-
-    //       cout << endl;
-    //       float sum = 0.0f;
-    //       for(int i=0; i<blocks; i++){
-    //           sum+=output3[i];
-    //       }
-    //       float result = expf(-r*T)*sum/N_PATHS;
-
-    //       cout << "time for simulation : " << milliseconds << " ms" << endl;
-    //       cout<< "result gpu cuda option price vanilla " << result << endl;
-
-    //       cudaFree(d_optionPriceGPU3);
-    //       cudaFree(d_output3);
-    //       free(output3);
-    //       cudaEventDestroy(start);
-    //       cudaEventDestroy(stop);
-    //     }
-    //     times_for_simulations[j] = mean/static_cast<float>(10);
-    //   }
-    //   fprintf(file, "%d, %f, %f, %f, %f, %f, %f\n", block_sizes[i], times_for_simulations[0], times_for_simulations[1], times_for_simulations[2], times_for_simulations[3], times_for_simulations[4], times_for_simulations[5]);
-    // }
-    // threads = (N_PATHS < maxThreads * 2) ? nextPow2((N_PATHS + 1) / 2) : maxThreads;
-    // blocks = (N_PATHS + (threads * 2 - 1)) / (threads * 2);
-
-
-    // float *output3, *d_optionPriceGPU3, *d_output3;
-    // output3 = (float *) malloc(blocks * sizeof(float));
-
-    // testCUDA(cudaMalloc((void **) &d_optionPriceGPU3, N_PATHS * sizeof(float)));
-    // testCUDA(cudaMalloc((void **) &d_output3, blocks * sizeof(float)));
-
-
-    // simulateOptionPriceMultipleBlockGPU<<<blocksPerGrid, threadsPerBlock>>>(d_optionPriceGPU3);
-    // cudaError_t error3 = cudaGetLastError();
-    // if (error3 != cudaSuccess) {
-    //     fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error3));
-    //     return -1;
-    // }
-
-
-    // reduce3<<<blocks, threads>>>(d_optionPriceGPU3, d_output3);
-    // error3 = cudaGetLastError();
-    // if (error3 != cudaSuccess) {
-    //     fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error3));
-    //     return -1;
-    // }
-
-
-    // cudaDeviceSynchronize();
-
-    // testCUDA(cudaMemcpy(output3, d_output3, blocks * sizeof(float), cudaMemcpyDeviceToHost));
-
-
-    // cout << endl;
-    // float sum = 0.0f;
-    // for (int i = 0; i < blocks; i++) {
-    //     sum += output3[i];
-    // }
-    // cout << "result gpu cuda option price vanilla " << expf(-r * T) * sum / N_PATHS << endl;
-
-    // cudaFree(d_optionPriceGPU3);
-    // cudaFree(d_output3);
-    // free(output3);
-
-
-
-
-
-
-
-//--------------------------------BLACK SCHOLES FORMULA ----------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------
 
     float callResult = 0.0f;
     black_scholes_CPU(callResult, option_data.S0, option_data.K, option_data.T, option_data.r, option_data.v);
