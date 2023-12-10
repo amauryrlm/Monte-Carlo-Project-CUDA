b051503d15eb59b8f87fbd8f4891058c173b5168
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-10 15:08:14 +0100

    up

--- hello.cu
+++ hello.cu
@@ -42,13 +42,13 @@ __constant__ OptionData d_OptionData;
 using namespace std;
 
 
-// Function that catches the error
 void testCUDA(cudaError_t error, const char *file, int line) {
     if (error != cudaSuccess) {
-        printf("There is an error in file %s at line %d\n", file, line);
+        std::cerr << "CUDA Error: " << cudaGetErrorString(error) << " in file " << file << " at line " << line << std::endl;
         exit(EXIT_FAILURE);
     }
 }
+
 // Has to be defined in the compilation in order to get the correct value of the
 // macros __FILE__ and __LINE__
 #define testCUDA(error) (testCUDA(error, __FILE__, __LINE__))
