bfb3ba27bcaab3656d039f279bff718e824aa4d2
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-10 20:08:36 +0100

    up

--- hello.cu
+++ hello.cu
@@ -552,6 +552,7 @@ void printOptionData(OptionData od) {
     cout << "P1 : " << od.P1 << endl;
     cout << "P2 : " << od.P2 << endl;
     cout << "N_PATHS : " << od.N_PATHS << endl;
+    cout << "N_PATHS_INNER : " << od.N_PATHS_INNER << endl;
     cout << "N_STEPS : " << od.N_STEPS << endl;
     cout << "step : " << od.step << endl;
     cout << endl;
@@ -599,6 +600,111 @@ void simulateOptionPriceCPU(float *optionPriceCPU, float *h_randomData, OptionDa
 
 __global__ void
 compute_nmc_one_block_per_point(float *d_option_prices, curandState *d_states, float *d_stock_prices, int *d_sums_i) {
+    int tid = threadIdx.x;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int blockSize = blockDim.x;
+    int blockId = blockIdx.x;
+
+    float K = d_OptionData.K;
+    float r = d_OptionData.r;
+    float sigma = d_OptionData.v;
+    float B = d_OptionData.B;
+    int P1 = d_OptionData.P1;
+    int P2 = d_OptionData.P2;
+    int N_PATHS = d_OptionData.N_PATHS;
+    int N_PATHS_INNER = d_OptionData.N_PATHS_INNER;
+    int N_STEPS = d_OptionData.N_STEPS;
+    float dt = d_OptionData.step;
+    float sqrdt = sqrtf(dt);
+
+    cg::thread_block cta = cg::this_thread_block();
+    __shared__ float sdata[1024];
+
+    long unsigned int number_of_simulations = N_PATHS * N_STEPS;
+    int number_of_blocks = gridDim.x;
+    curandState state = d_states[idx];
+
+    int count;
+    float St;
+    float G;
+    int remaining_steps;
+    tid = threadIdx.x;
+    int tid_sim;
+    while (blockId < number_of_simulations) {
+        remaining_steps = N_STEPS - ((blockId % N_STEPS) + 1);
+        float mySum = 0.0f;
+        tid_sim = tid;
+        while (tid_sim < N_PATHS_INNER) {
+
+            count = d_sums_i[blockId];
+            St = d_stock_prices[blockId];
+            for (int i = 0; i < remaining_steps; i++) {
+                G = curand_normal(&state);
+                St *= expf((r - (sigma * sigma) / 2) * dt + sigma * sqrdt * G);
+                if (B > St) count += 1;
+            }
+            if ((count >= P1) && (count <= P2)) {
+                mySum += max(St - K, 0.0f);
+
+
+            } else {
+                mySum += 0.0f;
+            }
+            tid_sim += blockSize;
+        }
+        sdata[tid] = mySum;
+        cg::sync(cta);
+        if ((blockSize >= 1024) && (tid < 512)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 512];
+        }
+        cg::sync(cta);
+        if ((blockSize >= 512) && (tid < 256)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 256];
+        }
+
+        cg::sync(cta);
+
+        if ((blockSize >= 256) && (tid < 128)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 128];
+        }
+
+        cg::sync(cta);
+
+        if ((blockSize >= 128) && (tid < 64)) {
+            sdata[tid] = mySum = mySum + sdata[tid + 64];
+        }
+        cg::sync(cta);
+
+
+        cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
+
+        if (cta.thread_rank() < 32) {
+            // Fetch final intermediate sum from 2nd warp
+            if (blockSize >= 64) mySum += sdata[tid + 32];
+            // Reduce final warp using shuffle
+            for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
+                mySum += tile32.shfl_down(mySum, offset);
+            }
+        }
+
+        // write result for this block to global mem
+        if (cta.thread_rank() == 0) {
+            //atomic add
+            mySum = mySum * expf(-r) / static_cast<float>(N_PATHS_INNER);
+            atomicAdd(&(d_option_prices[blockId]), mySum);
+
+        }
+        // if (cta.thread_rank() == 0 && blockId < number_of_simulations && blockId > (number_of_simulations - 100))  printf("blockId : %d, d_option_prices[blockId] : %f, d_sums_i[blockId] : %d, d_stock_prices[blockId] : %f, remaining_steps : %d\n", blockId, d_option_prices[blockId], d_sums_i[blockId], d_stock_prices[blockId], remaining_steps); 
+
+        blockId += number_of_blocks;
+    }
+
+
+}
+
+__global__ void
+compute_nmc_one_block_per_point_with_outter(float *d_option_prices, curandState *d_states, float *d_stock_prices,
+                                            int *d_sums_i) {
     int tid = threadIdx.x;
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int blockSize = blockDim.x;
@@ -846,7 +952,6 @@ wrapper_gpu_bullet_option_nmc_one_point_one_block(OptionData option_data, int th
     CHECK_MALLOC(h_stock_prices);
     CHECK_MALLOC(h_sums_i);
 
-    
 
     testCUDA(cudaMalloc(&d_states_outter, N_PATHS * sizeof(curandState)));
     setup_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_states_outter, 1234);
@@ -871,10 +976,9 @@ wrapper_gpu_bullet_option_nmc_one_point_one_block(OptionData option_data, int th
     testCUDA(cudaMemGetInfo(&freeMem2, &totalMem2));
 
 
-
-    std::cout << "Free memory 2 : " << freeMem2 / 1024 / 1024 << " MB\n";
-    std::cout << "Total memory 2 : " << totalMem2 / 1024 / 1024 << " MB\n";
-    std::cout << "Used memory 2 : " << (totalMem2 - freeMem2) / 1024 / 1024 << " MB\n";
+    std::cout << "Free memory : " << freeMem2 / 1024 / 1024 << " MB\n";
+    std::cout << "Total memory : " << totalMem2 / 1024 / 1024 << " MB\n";
+    std::cout << "Used memory : " << (totalMem2 - freeMem2) / 1024 / 1024 << " MB\n";
 
 
     compute_nmc_one_block_per_point<<<number_of_blocks, threadsPerBlock>>>(d_option_prices, d_states_inner,
@@ -891,13 +995,22 @@ wrapper_gpu_bullet_option_nmc_one_point_one_block(OptionData option_data, int th
     for (int i = 0; i < number_of_options; i++) {
         sum += h_option_prices[i];
     }
-    cout << "Average GPU bullet option nmc one point per block : " << sum / static_cast<float>(number_of_options)
+    float callResult = sum / static_cast<float>(number_of_options);
+    cout << "Average GPU bullet option nmc one point per block : " << callResult
          << endl << endl;
 
 
-    return h_option_prices[N_PATHS * N_STEPS];
+    free(h_option_prices);
+    free(h_stock_prices);
+    free(h_sums_i);
+    cudaFree(d_option_prices);
+    cudaFree(d_stock_prices);
+    cudaFree(d_sums_i);
+
+    return callResult;
 
 }
+
 float
 get_max_number_of_blocks(OptionData option_data, int threadsPerBlock) {
 
@@ -919,7 +1032,6 @@ get_max_number_of_blocks(OptionData option_data, int threadsPerBlock) {
     CHECK_MALLOC(h_stock_prices);
     CHECK_MALLOC(h_sums_i);
 
-    
 
     testCUDA(cudaMalloc(&d_states_outter, N_PATHS * sizeof(curandState)));
     setup_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_states_outter, 1234);
@@ -935,11 +1047,6 @@ get_max_number_of_blocks(OptionData option_data, int threadsPerBlock) {
     size_t freeMem;
     size_t totalMem;
     testCUDA(cudaMemGetInfo(&freeMem, &totalMem));
-
-
-    std::cout << "Free memory 1 : " << freeMem / 1024 / 1024 << " MB\n";
-    std::cout << "Total memory 1 : " << totalMem / 1024 / 1024 << " MB\n";
-    std::cout << "Used memory 1 : " << (totalMem - freeMem) / 1024 / 1024 << " MB\n";
 
     int number_of_blocks = 10000;
     cudaError_t status;
