967feaba278072c918cbb34de1bf6aaf2a984231
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-10 21:44:30 +0100

    up

--- hello.cu
+++ hello.cu
@@ -1031,7 +1031,7 @@ int main(void) {
     option_data.B = 120.0f;
     option_data.P1 = 10;
     option_data.P2 = 50;
-    option_data.N_PATHS = 100000;
+    option_data.N_PATHS = 1000000;
     option_data.N_PATHS_INNER = 10000;
     option_data.N_STEPS = 100;
     option_data.step = option_data.T / static_cast<float>(option_data.N_STEPS);
@@ -1050,11 +1050,11 @@ int main(void) {
 
     // wrapper_gpu_bullet_option(option_data, threadsPerBlock);
     // wrapper_gpu_bullet_option_atomic(option_data, threadsPerBlock);
-    // int max_number_of_block_to_everfow = get_max_number_of_blocks(option_data, threadsPerBlock);
+    int max_number_of_block_to_everfow = get_max_number_of_blocks(option_data, threadsPerBlock);
     // wrapper_gpu_bullet_option_nmc_one_point_one_block(option_data, threadsPerBlock, 250000);
 
 
-    wrapper_gpu_bullet_option_nmc_one_kernel(option_data, threadsPerBlock, 250000);
+    // wrapper_gpu_bullet_option_nmc_one_kernel(option_data, threadsPerBlock, 250000);
 
 
     float callResult = 0.0f;
