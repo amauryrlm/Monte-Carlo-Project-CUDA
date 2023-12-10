32f0b5b60d575757cc002ad2401859c09f2c59b8
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-10 16:21:59 +0100

    f

--- hello.cu
+++ hello.cu
@@ -882,7 +882,7 @@ wrapper_gpu_bullet_option_nmc_one_point_one_block(OptionData option_data, int th
         }
     }
     cout << "max number of blocks : " << number_of_blocks << endl;
-    cudaMalloc(&d_states_inner, number_of_blocks * threadsPerBlock * sizeof(curandState))
+    cudaMalloc(&d_states_inner, number_of_blocks * threadsPerBlock * sizeof(curandState));
 
     setup_kernel<<<number_of_blocks, threadsPerBlock>>>(d_states_inner, 1235);
     testCUDA(cudaGetLastError());
