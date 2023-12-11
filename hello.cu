a260143628cdc374340fc9d90a74deee561b48d8
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-11 21:36:04 +0100

    up

--- hello.cu
+++ hello.cu
@@ -1045,6 +1045,7 @@ compute_nmc_one_block_per_point_with_outter(float *d_option_prices, curandState 
                 //atomic add
                 mySum = mySum * expf(-r) / static_cast<float>(N_PATHS_INNER);
                 atomicAdd(&(d_option_prices[blockId]), mySum);
+                printf("blockId : %d, d_option_prices[blockId] : %f, d_sums_i[blockId] : %d, d_stock_prices[blockId] : %f, remaining_steps : %d\n", blockId, d_option_prices[blockId], d_sums_i[blockId], d_stock_prices[blockId], remaining_steps);
             }
         }
         compteur += 1;
