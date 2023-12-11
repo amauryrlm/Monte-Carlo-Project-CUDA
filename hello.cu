129f4e191bdf982203bf463663121d3abd76cdc6
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-11 22:57:55 +0100

    up

--- hello.cu
+++ hello.cu
@@ -299,8 +299,8 @@ int main(void) {
 
     int number_blocks = get_max_blocks(threadsPerBlock);
     printf("Computing nmc option price with %lu blocks.\n", number_blocks);
-    wrapper_gpu_bullet_option_nmc_one_point_one_block(option_data, threadsPerBlock, number_blocks);
-    // wrapper_gpu_bullet_option_nmc_one_kernel(option_data, threadsPerBlock, number_blocks);
+    // wrapper_gpu_bullet_option_nmc_one_point_one_block(option_data, threadsPerBlock, number_blocks);
+    wrapper_gpu_bullet_option_nmc_one_kernel(option_data, threadsPerBlock, number_blocks);
 
     float callResult = 0.0f;
     black_scholes_CPU(callResult, option_data.S0, option_data.K, option_data.T, option_data.r, option_data.v);
