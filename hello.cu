fa797e4664649b6a0d42185d38043d3dc161963f
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2023-12-11 23:08:40 +0100

    up

--- hello.cu
+++ hello.cu
@@ -1119,7 +1119,7 @@ int main(void) {
     option_data.P1 = 10;
     option_data.P2 = 50;
     option_data.N_PATHS = 100000;
-    option_data.N_PATHS_INNER = 100000;
+    option_data.N_PATHS_INNER = 1000;
     option_data.N_STEPS = 100;
     option_data.step = option_data.T / static_cast<float>(option_data.N_STEPS);
 
